e7bd59328cabc862603cde306cead662ad586079
jerusalemdax <jerusalemdax@gmail.com>
2016-05-12 15:14:06 +0800

    添加项目文件。

---
+++ src/WebManager/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebManager.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
